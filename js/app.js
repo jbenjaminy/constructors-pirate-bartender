ea84ad0576e5b9129a1a53cd0ef201d9dc262420
jbenjaminy <young.bejamin@gmail.com>
2016-06-20 18:05:12 -0500

    Adds final stylistic changes

--- js/app.js
+++ js/app.js
@@ -1,29 +1,32 @@
 $(document).ready( function(){
-var newDrink = [];
-var ingredients = {
 
+// VARIABLES
+var newDrink = [];
+
+var ingredients = {
     strong: ["Glug of rum", "Slug of whisky", "Splash of gin"],
     salty: ["Olive on a stick", "Salt-dusted rim", "Rasher of bacon"],
     bitter: ["Shake of bitters", "Splash of tonic", "Twist of lemon peel"],
     sweet: ["Sugar cube", "Spoonful of honey", "Splash of cola"],
     fruity: ["Slice of orange", "Dash of cassis", "Cherry on top"],
-
 };
 
+// MAKE DRINK CLICK FUNCTION
 	$('#make-drink').click(function(){
 		makeDrink();
 	});
 
-// Function drink creation
+// DRINK CREATION FUNCTION
 	function makeDrink() {
+		// TAKES USER INPUT PREFERENCES AND STORES THEM IN NEW DRINK ARRAY
 		var drinkPreferences = $('#questions-list').find('input[name=questions-input]:checked');
 		newDrink = [];
+		// RANDOMIZED SELECTION OF AN INGREDIENT FROM EACH CATEGORY
 		$.each(drinkPreferences, function(index, element) {
 			newDrink.push(ingredients[element.value][
 				Math.floor((Math.random() * 3))]);
-				
 		});
-		console.log(newDrink);	
+		// DISPLAYS THE FINISHED DRINK RECIPE AND PLAYS SOUND CLIP
 		$('.main').hide();
 		$('.finished-drinks').show();
 		var drinkName = "";
@@ -34,18 +37,18 @@ var ingredients = {
 			soundClip();
 	}
 
+// CLICK FUNCTION TO RESET SELECTION AND MAKE NEW DRINK 
 	$('body').on('click', '#new-drink', function(event){
 		$('.main').hide();
 		$('.questions').show();
 		$('input[type=checkbox]').prop('checked', false);
-		$("#pirate-song")[0].stop();
 	});
 
-
-function soundClip() {
-    $("#pirate-song")[0].volume = 0.2;
-    $("#pirate-song")[0].load();
-    $("#pirate-song")[0].play();
-};
+// SOUND CLIP LOAD AND PLAY FUNCTION
+	function soundClip() {
+	    $("#pirate-song")[0].volume = 0.5;
+	    $("#pirate-song")[0].load();
+	    $("#pirate-song")[0].play();
+	};
 
 });
