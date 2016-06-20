b89f21e74cdff217f471e2a58c2dc8c49f88b417
jbenjaminy <young.bejamin@gmail.com>
2016-06-20 16:45:54 -0500

    Adds final JS changes and CSS styling

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ $(document).ready( function(){
 var newDrink = [];
 var ingredients = {
 
-    strong: ["Glug of rum", "slug of whisky", "splash of gin"],
-    salty: ["Olive on a stick", "salt-dusted rim", "rasher of bacon"],
-    bitter: ["Shake of bitters", "splash of tonic", "twist of lemon peel"],
-    sweet: ["Sugar cube", "spoonful of honey", "splash of cola"],
-    fruity: ["Slice of orange", "dash of cassis", "cherry on top"],
+    strong: ["Glug of rum", "Slug of whisky", "Splash of gin"],
+    salty: ["Olive on a stick", "Salt-dusted rim", "Rasher of bacon"],
+    bitter: ["Shake of bitters", "Splash of tonic", "Twist of lemon peel"],
+    sweet: ["Sugar cube", "Spoonful of honey", "Splash of cola"],
+    fruity: ["Slice of orange", "Dash of cassis", "Cherry on top"],
 
 };
 
@@ -14,8 +14,7 @@ var ingredients = {
 		makeDrink();
 	});
 
-// Function driyounk creation
-	// take user input
+// Function drink creation
 	function makeDrink() {
 		var drinkPreferences = $('#questions-list').find('input[name=questions-input]:checked');
 		newDrink = [];
@@ -25,28 +24,28 @@ var ingredients = {
 				
 		});
 		console.log(newDrink);	
-
-		// If we get a response from the checkbox === 'on'
-			// Then, we will locate where that 'on' is coming from (what ingredient)
-				// and then mix it up
+		$('.main').hide();
+		$('.finished-drinks').show();
+		var drinkName = "";
+		for (var i = 0; i < newDrink.length; i++) {
+			drinkName += newDrink[i] + " ";
+		}
+		$('#drink').text("Here's your" + " " + drinkName + " " + "Brew!");
+			soundClip();
 	}
-		// for all yesses randomly select ingredient from category matching that key
-			// randomNumber = Math.floor((Math.random() * 100) + 1);
-	// add all ingredients selected to drink array
-	// return statement
-// "Here's your" + ingredient + "brew."
 
-// $('#questions-list').append("<li>" + "Do ye like yer drinks strong?" + "<input type="checkbox"></li>
-// 			<li>Do ye like it with a salty tang?<input type="checkbox"></li>
-// 			<li>Are ye a lubber who likes it bitter?<input type="checkbox"></li>
-// 			<li>Would ye like a bit of sweetness with yer poison?<input type="checkbox"></li>
-// 			<li>Are ye one for a fruity finish?<input type="checkbox"></li>
+	$('body').on('click', '#new-drink', function(event){
+		$('.main').hide();
+		$('.questions').show();
+		$('input[type=checkbox]').prop('checked', false);
+		$("#pirate-song")[0].stop();
+	});
 
-// var drinkQuestions = {
-//    	Question1: "Do ye like yer drinks strong?",
-//    	Question2: "Do ye like it with a salty tang?",
-//     Question3: "Are ye a lubber who likes it bitter?",
-//     Question4: "Would ye like a bit of sweetness with yer poison?",
-//     Question5: "Are ye one for a fruity finish?"
-// }
+
+function soundClip() {
+    $("#pirate-song")[0].volume = 0.2;
+    $("#pirate-song")[0].load();
+    $("#pirate-song")[0].play();
+};
+
 });
